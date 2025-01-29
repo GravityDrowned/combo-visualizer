23be3e0557b4c7e59bc18ff405bbfacf051fd45e
Michael Wagner <michael.wagner@codecentric.de>
2025-01-29 18:44:03 +0100

    image to looky looky

--- crawler.py
+++ crawler.py
@@ -30,17 +30,25 @@ def crawl(response):
         combo_results = soup.find_all('a', class_='comboResults_comboResult__VcfMx')
         for combo in combo_results:
             cards_html = combo.find_all('div', class_="card-name")
+            images_html = combo.find_all('div', class_='cardTooltip_cardTooltip__3eItj')
+            images_html2 =  combo.find_all('img')
             results_html = combo.find_all('div', class_="result")
 
             cards = []
+            images = []
             results = []
+
             for i, card in enumerate(cards_html):
                 cards.append(card.text.strip())
+            for i, image in enumerate(images_html):
+                src = image.find('img')['src']
+                images.append(src)
             for i, result in enumerate(results_html):
                 results.append(result.text.strip())
 
             combo = {
                 "cards": cards,
+                "images": images,
                 "results": results
             }
             print(combo)
@@ -51,6 +59,5 @@ def crawl(response):
         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
 
 url = "https://commanderspellbook.com/search/?q=Xyris+ci%3ATemur"
-url = "https://commanderspellbook.com/search/?q=Intruder+Alarm+ci%3ATemur"
 
 crawl_until_the_end(url)
--- visualization.py
+++ visualization.py
@@ -12,26 +12,31 @@ def add_all_edges(net, cards, results):
             net.add_edge(card, other_card, color=color, title='\n'.join(results))
 
 def create_graph(combos):
-    net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white", notebook=True, select_menu=True,
+    net = Network(height="1500px", width="100%", bgcolor="#222222", font_color="white", notebook=True, select_menu=True,
                   filter_menu=True)
     net.barnes_hut()
     if not combos:
         return
     for combo in combos:
         cards = combo.get("cards")
-        net.add_nodes(cards, title=cards)
+        images = combo.get("images")
+        for i, card in enumerate(cards):
+            net.add_node(card, title=card, shape="image", image=images[i], size=20)
         add_all_edges(net, cards, combo.get("results"))
-    net.show_buttons(filter_=['physics'])
+
+    net.show_buttons()
     net.toggle_physics(True)
     net.show("cards.html")
 
 cards_mock = ['Xyris, the Writhing Storm', 'Intruder Alarm', 'Lore Broker']
+images_mock = ['https://api.scryfall.com/cards/named?format=image&version=normal&exact=Xyris%2C%20the%20Writhing%20Storm&face=front', 'https://api.scryfall.com/cards/named?format=image&version=normal&exact=Intruder%20Alarm&face=front', 'https://api.scryfall.com/cards/named?format=image&version=normal&exact=Lore%20Broker&face=front']
 results_mock = ['Infinite draw triggers for all players', 'Infinite looting for all players',
                     'Infinite self-discard triggers for all players', 'Near-infinite creature tokens',
                     'Near-infinite ETB', 'Near-infinite untap of all creatures']
 
 t = {
     "cards": cards_mock,
+    "images": images_mock,
     "results": results_mock
 }
 
